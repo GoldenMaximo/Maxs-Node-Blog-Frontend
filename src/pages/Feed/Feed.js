7c10f6d0ae2f4af9bfe8d1de545282307ca6283d
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-03 01:16:38 -0300

    feat: Add pagination.

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -58,7 +58,7 @@ class Feed extends Component {
         const graphqlQuery = {
             query: `
                 {
-                    posts {
+                    posts(page: ${page}) {
                         posts {
                             _id
                             title
@@ -208,6 +208,7 @@ class Feed extends Component {
                         );
                         updatedPosts[postIndex] = post;
                     } else {
+                        updatedPosts.pop();
                         updatedPosts.unshift(post);
                     }
                     return {
