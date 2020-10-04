e62a3df0ba6bb4582874e0e26feb48a84945adf2
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-04 14:34:32 -0300

    feat: Get and update User status.

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,19 +22,30 @@ class Feed extends Component {
     };
 
     componentDidMount() {
-        fetch('http://localhost:8080/status', {
+        const graphqlQuery = {
+            query: `
+                {
+                    user {
+                        status
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            body: JSON.stringify(graphqlQuery),
             headers: {
-                Authorization: `Bearer ${this.props.token}`
+                Authorization: `Bearer ${this.props.token}`,
+                "Content-Type": 'application/json',
             }
         })
-            .then(res => {
-                if (res.status !== 200) {
-                    throw new Error('Failed to fetch user status.');
-                }
-                return res.json();
-            })
+            .then(res => res.json())
             .then(resData => {
-                this.setState({ status: resData.status });
+                if (resData.errors) {
+                    throw new Error("Fetching status failed!");
+                }
+                this.setState({ status: resData.data.user.status });
             })
             .catch(this.catchError);
 
@@ -107,24 +118,30 @@ class Feed extends Component {
 
     statusUpdateHandler = event => {
         event.preventDefault();
-        fetch('http://localhost:8080/status', {
-            method: 'PUT',
-            body: JSON.stringify({
-                status: this.state.status
-            }),
+
+        const graphqlQuery = {
+            query: `
+                mutation {
+                    updateStatus(status: "${this.state.status}") {
+                        status
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            body: JSON.stringify(graphqlQuery),
             headers: {
                 Authorization: `Bearer ${this.props.token}`,
                 "Content-Type": 'application/json',
             }
         })
-            .then(res => {
-                if (res.status !== 200 && res.status !== 201) {
-                    throw new Error("Can't update status!");
-                }
-                return res.json();
-            })
+            .then(res => res.json())
             .then(resData => {
-                console.log(resData);
+                if (resData.errors) {
+                    throw new Error("Updating status failed!");
+                }
             })
             .catch(this.catchError);
     };
